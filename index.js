9aedcc80f4ed4ff1bb8def87e680c25d6cd04c16
ander7agar <ander7agar@gmail.com>
2018-05-12 00:35:04 +0200

    fix import unblockContent

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let {Constants} = require('./constants');
 let {TrantorNetwork} = require('./network');
 let {TrantorUtils} = require('./utils');
 let {ContentData, Index, Author, MediaData, Like, Unlike, Payment, Comment, Donation, AddressRelation, Follow,
-    Unfollow, BlockContent} = require('./content');
+    Unfollow, BlockContent, UnblockContent} = require('./content');
 
 module.exports = {
     Constants, TrantorNetwork, TrantorUtils, ContentData, Index, Author, MediaData, Like, Unlike, Payment, Comment,
-    Donation, AddressRelation, Follow, Unfollow, BlockContent
+    Donation, AddressRelation, Follow, Unfollow, BlockContent, UnblockContent
 };
